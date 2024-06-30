06b6141041b66481783a1dcc170a4c0eb429b275
Claire Le <cjames96.uc@gmail.com>
2024-06-29 23:19:10 -0400

    First push!

---
+++ LearningPython/initials.py
@@ -0,0 +1,22 @@
+initial1 = """
+ CCC 
+C   C
+C     
+C    
+C    
+C    C
+ CCC  
+"""
+initial2 = """
+JJJJJ
+  J  
+  J
+  J
+J J
+J J
+ JJ
+"""
+print(initial1 + initial2)
+
+#comment1: Claire James
+#comment2: I like to stretch my legs.
